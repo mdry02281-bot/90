146e0eb7507e2e5f02b89ade60eddbcd2d6028bf
PromoHive <promohive@globalpromonetwork.store>
2025-10-27 12:30:45 +0300

    Fix AdminDashboard import and complete admin panel functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Route, Switch } from 'wouter'
+import AdminDashboard from './pages/admin/AdminDashboard'
 
 // Notification Context Types
 interface NotificationContextType {
